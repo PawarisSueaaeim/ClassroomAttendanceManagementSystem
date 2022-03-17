96e2adc9af34502f40e623008ede481632a95a10
PawarisSueaaeim <97650022+PawarisSueaaeim@users.noreply.github.com>
2022-03-17 10:04:09 +0700

    first commit

---
+++ manageClassroomWeb-main/src/screens/components/SidebarData.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import * as AiIcons from 'react-icons/ai';
+import * as IoIcons from 'react-icons/io';
+import * as MdIcons from 'react-icons/md';
+import * as BsIcons from 'react-icons/bs';
+
+export const SidebarData = [
+  {
+    title: 'หน้าแรก',
+    path: '/',
+    icon: <AiIcons.AiFillHome style={{ color: '#9F76B4'}}/>,
+    cName: 'nav-text'
+  },
+  {
+    title: 'สร้างรายงาน',
+    path: '/reports',
+    icon: <IoIcons.IoIosPaper style={{ color: '#9F76B4'}}/>,
+    cName: 'nav-text'
+  },
+];
